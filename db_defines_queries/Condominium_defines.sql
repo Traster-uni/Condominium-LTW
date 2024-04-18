c922841c3e58806a7d299265a5f0df1c9d9ce1e0
Tommaso M <84441861+Traster-uni@users.noreply.github.com>
2024-04-18 11:54:38 +0200

    modifieds fields and type definitions, defined all the remaining tables.

--- db_defines_queries/Condominium_defines.sql
+++ db_defines_queries/Condominium_defines.sql
@@ -8,9 +8,16 @@ CHECK(
 	VALUE ~ '^\d{5}$'
 );
 
+CREATE DOMAIN email as varchar(50)
+CHECK(
+	VALUE ~ '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$'
+);
+
 CREATE TYPE ut_reqest_stat AS ENUM ('accepted', 'refused', 'aborted');
 
-CREATE TYPE aptBlock_request_stat AS ENUM ('accepted', 'refused');
+CREATE TYPE request_status AS ENUM ('accepted', 'refused');
+-- may needs deletion and creation, ALSO THE TABLE THAT USE THIS TYPE MAY NEED TO BE DROPPED FIRST
+-- DROP DOMAIN IF EXISTS request_status 
 
 CREATE TABLE IF NOT EXISTS ut_no_reg (
 	cookie integer check (cookie >= 0), -- cookie maybe random, maybe always non negative
@@ -32,11 +39,19 @@ CREATE TABLE IF NOT EXISTS ut_registered(
 
 CREATE TABLE IF NOT EXISTS ut_owner(
 	ut_id integer,
+	ut_name varchar(50) NOT NULL,
+	ut_surname varchar(50)NOT NULL,
+	ut_tel telNumber NOT NULL,
+	ut_email email NOT NULL,
+	passwd_hash varchar(128) NOT NULL, --store the hash not the actual passwd
 	ut_doc_fname varchar(100) NOT NULL,
 	ut_doc_purchase bytea NOT NULL,
 	PRIMARY KEY (ut_id),
 	FOREIGN KEY (ut_id) REFERENCES ut_registered(ut_id)
 );
+-- hash algorithms: https://security.stackexchange.com/questions/211/how-to-securely-hash-passwords
+-- may needs deletion and creation
+-- DROP TABLE IF EXISTS ut_owner CASCADE 
 
 CREATE TABLE IF NOT EXISTS aptBlock_admin(
 	ut_id integer,
@@ -66,27 +81,29 @@ CREATE TABLE IF NOT EXISTS req_aptBlock_create(
 	aptBlockReq_id serial,
 	time_born timestamp,
 	time_mod timestamp,
-	status aptBlock_request_stat,
+	stat request_status,
 	PRIMARY KEY (aptBlockReq_id),
 	FOREIGN KEY (ut_id) REFERENCES aptBlock_admin(ut_id)
 );
-
+-- may needs deletion and creation
+-- DROP TABLE IF EXISTS req_aptBlock_create CASCADE
 CREATE TABLE IF NOT EXISTS aptBlock(
 	aptBlock_id integer,
-	address varchar(50) NOT NULL,
+	addr_aptB varchar(50) NOT NULL,
 	postalCode postalCode NOT NULL,
 	city varchar(50),
 	PRIMARY KEY (aptBlock_id),
 	FOREIGN KEY (aptBlock_id) REFERENCES req_aptBlock_create(aptBlockReq_id),
 	FOREIGN KEY (city) REFERENCES city(name),
-	UNIQUE (address, city)
+	UNIQUE (addr_aptB, city)
 );
-
+-- may needs deletion and creation
+-- DROP TABLE IF EXISTS aptBlock CASCADE
 CREATE TABLE IF NOT EXISTS ut_personal_documents(
 	ut_id integer,
 	expr_date_ID date NOT NULL,
-	img_ID bytea NOT NULL,
-	img_ID_fname varchar(100) NOT NULL,
+	img_ID bytea[] NOT NULL,
+	img_ID_fname varchar(100)][] NOT NULL,
 	img_FiscalCode bytea NOT NULL,
 	PRIMARY KEY (ut_id),
 	FOREIGN KEY (ut_id) REFERENCES ut_registered(ut_id)
@@ -117,14 +134,15 @@ CREATE TABLE IF NOT EXISTS aptBlock_bulletinBoard(
 	FOREIGN KEY (aptBlock_id) REFERENCES aptBlock(aptBlock_id)
 );
 
-CREATE TABLE IF NOT EXISTS post(
+CREATE TABLE IF NOT EXISTS posts(
 	post_id serial,
-	bb_id integer,	-- bullettin board where the post is pinned to
+	bb_id integer,		-- bullettin board where the post is pinned to
 	ut_owner_id integer,
 	title varchar(100) NOT NULL,
-	ttext text NOT NULL,
-	time_born timestamp NOT NULL, -- current_time
-	time_edit timestamp NOT NULL,
+	ttext text[] NOT NULL,
+	time_born timestamp NOT NULL, 	-- current_time
+	time_edit timestamp NOT NULL,	-- current_time at time of last modification
+	data_json json,		-- to be defined: JSON module for polls and JSON module for payments
 	off_comments bool DEFAULT false,
 	PRIMARY KEY (post_id),
 	FOREIGN KEY (bb_id) REFERENCES aptBlock_bulletinBoard(bb_id),
@@ -132,12 +150,14 @@ CREATE TABLE IF NOT EXISTS post(
 	UNIQUE (post_id, bb_id)
 );
 
-CREATE TABLE IF NOT EXISTS post_thread(
+-- may needs deletion and creation
+-- DROP TABLE IF EXISTS post_thread CASCADE
+CREATE TABLE IF NOT EXISTS post_thread(		
 	thread_id serial,
 	ud_id integer,
 	post_id integer,	-- therad is related to a certain post
-	comm_text text,
-	time_born timestamp NOT NULL, -- current_time
+	comm_text text[],
+	time_born timestamp NOT NULL, 		-- current_time
 	time_lastReplay timestamp NOT NULL, -- current_time last reply
 	PRIMARY KEY (thread_id),
 	FOREIGN KEY (post_id) REFERENCES post(post_id),
@@ -145,6 +165,7 @@ CREATE TABLE IF NOT EXISTS post_thread(
 	UNIQUE (thread_id, post_id)
 );
 
+-- usde for post_thread and tickets
 CREATE TABLE IF NOT EXISTS reply_thread(
 	thread_id integer,
 	ud_id integer,
@@ -153,7 +174,64 @@ CREATE TABLE IF NOT EXISTS reply_thread(
 	FOREIGN KEY (ud_id) REFERENCES ut_owner(ut_id)
 );
 
-CREATE TABLE IF NOT EXISTS post_plugin();
+CREATE TABLE IF NOT EXISTS tags(
+	name_tag varchar(10) PRIMARY KEY
+);
 
-CREATE TABLE IF NOT EXISTS tags();
+CREATE TABLE IF NOT EXISTS tags_posts(
+	name_tag varchar(10),
+	post_id integer,
+	PRIMARY KEY (name_tag, post_id),
+	FOREIGN KEY (name_tag) REFERENCES tags(name_tag),
+	FOREING KEY (post_id) REFERENCES posts(post_id)
+);
 
+-- tickets are a special kind of threads
+CREATE TABLE IF NOT EXISTS tickets(		
+	ticket_id serial,
+	ud_id integer,
+	aptBlock_admin integer,	-- therad is related to a certain post
+	title varchar(50) NOT NULL,
+	comm_text text[] NOT NULL,
+	imgs_data bytea[],
+	imgs_fname varchar(100)[],
+	time_born timestamp NOT NULL, 		-- current_time
+	time_lastReplay timestamp NOT NULL, -- current_time last reply
+	PRIMARY KEY (thread_id),
+	FOREIGN KEY (aptBlock_admin) REFERENCES aptBlock_admin(ut_id),
+	FOREIGN KEY (ud_id) REFERENCES ut_owner(ut_id),
+);
+-- TRIGGER: max 5 tickets per ud_id
+
+CREATE TABLE IF NOT EXISTS tags_tickets(
+	name_tag varchar(10),
+	ticket_id integer,
+	PRIMARY KEY (name_tag ticket_id),
+	FOREIGN KEY (name_tag) REFERENCES tags(name_tag),
+	FOREING KEY (ticket_id) REFERENCES tickets(ticket_id)
+);
+ 
+ CREATE TABLE IF NOT EXISTS common_spaces(
+	cs_id serial,
+	common_space_name varchar(50) NOT NULL,
+	int_num integer NOT NULL,
+	floor_num integer NOT NULL,
+	PRIMARY KEY (common_space_name)
+ );
+
+ CREATE TABLE IF NOT EXISTS rental_request(
+	rental_req_id serial,
+	ut_id integer,
+	adm_id integer,
+	rental_time time NOT NULL, 
+	rental_day day CHECK (rental_day > current_date) NOT NULL, 
+	retal_period integer CHECK (retal_period > 0),
+	submit_time timestamp NOT NULL,
+	stat request_status NOT NULL,
+	PRIMARY KEY (rental_req_id),
+	FOREIGN KEY (ut_id) REFERENCES ut_owner(ut_id),
+	FOREIGN KEY (adm_id) REFERENCES aptBlock_admin(ut_id)
+ );
+ -- TRIGGER: max n rental_req accepted per user
+ -- TRIGGER: rental_req acceptable if within x days from current_date
+ -- TRIGGER: for each user there can't be multiple rental_req in the same period/day
