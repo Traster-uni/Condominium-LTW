8ec2addf5be5261736abd405227a2260abcec2cb
Rineva <rinaldevangelista@live.it>
2024-04-19 10:41:41 +0200

    aggiustamenti area utente

--- ext_resources/04-js/tabs.js
+++ ext_resources/04-js/tabs.js
@@ -20,8 +20,3 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
-
-document.getElementById("defaultOpen").click();
-
-
-
