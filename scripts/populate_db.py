b7b8c64b64c2335140de9488a9936f676efbb651
Tommaso M <84441861+Traster-uni@users.noreply.github.com>
2024-04-23 20:49:38 +0200

    script update

--- scripts/populate_db.py
+++ scripts/populate_db.py
@@ -1,6 +1,27 @@
-import psycopg2
 import os
-from config import load_config
+import json
+import psycopg2
+from configparser import ConfigParser
+
+# CONFIGURATION LOADER
+def load_config(filename="database.ini", section="postgresql"):
+    parser = ConfigParser()
+    fname_abs = os.path.dirname(os.path.abspath(__file__))
+    parser.read(os.path.join(fname_abs, filename))
+    
+    config = dict()
+    if parser.has_section(section):
+        params = parser.items(section)
+        for key,value in params:
+            if key == "port":
+                config[key] = int(value)
+            else:
+                config[key] = value
+    else:
+        raise Exception(f"Section {section} not found in the {filename} file")
+
+    return config
+
 
 def connect(config):
     print(f"connect ---> {config}")
@@ -11,9 +32,28 @@ def connect(config):
             return conn
     except (psycopg2.DatabaseError, Exception) as err:
         print(err)
+    return
+
+
+def load_data(fname="data.json"):
+    data = dict()
+    fname_abs = os.path.dirname(os.path.abspath(__file__))
+
+    with open(os.path.join(fname_abs, fname)) as f:
+        data = json.load(f)   
+    return data
+
+def insert_data(data:dict, connection:psycopg2.connect):
+    cur = connection.cursor()
+
+    for table_name in data:
+        for instance in table_name:
+            cur.execute(f"INSERT {table_name}")
 
 if __name__ == '__main__':
     config = load_config("database.ini")
-    connect(config)
+    conn = connect(config)
+    d = load_data("default_schema_values")
+    insert_data(d, conn)
 
 # from https://www.postgresqltutorial.com/postgresql-python/connect/
