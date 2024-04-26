5e9cec0e02af65fc3e5b58af7e11b26cb4d3b094
Traster-uni <lopedote.1942920@studenti.uniroma1.it>
2024-04-26 20:20:53 +0200

    updated query malformed

--- db_defines_queries/Condominium_triggers.sql
+++ db_defines_queries/Condominium_triggers.sql
@@ -1,14 +1,14 @@
 CREATE OR REPLACE FUNCTION max_rental_req_accepted_per_user() RETURNS trigger
 AS $$
 	usr_id = TD["new"]["ud_id"]
-	rt_day = TD["new"]["rental_req_id"]
+	rt_id = TD["new"]["rental_req_id"]
 	print(f"usr_id: {usr_id}, rental_day: {rt_day}")
 	qry = f"""
-	        SELECT r_req.ud_id, COUNT() as c
+	        SELECT count(r_req.rental_req_id)
 	        FROM rental_requests r_req
-	        WHERE r_req.ud_id == {usr_id} AND r_req stat=='pending' AND
-	             date_part(current_date, 'day') - date_part({rt_day}, 'day') <= 30
-	        GROUP BY (r_req.ud_id)
+	        WHERE r_req.ud_id == {usr_id} AND (r_req.stat=='pending' OR r_req.stat=='accepted')
+				AND date_trunc('day', r_req.rental_time) > date_trunc('day', CURRENT_TIMESTAMP)
+	        GROUP BY (r_req.rental_req_id)
 	        """
 	plpython3u.prepare(qry)
 	try:
@@ -16,8 +16,8 @@ AS $$
 	except plpy.SPIError:
 	    return "something went wrong"
 	else:
-	    if qry_result[0]["c"] > 5:
-	        raise plpy.error(f"Max num of requests reached for {usr_id}")
+	    if qry_result[0] > 5:
+	        raise plpy.error(f"Max num of requests reached for {usr_id}, rolling back")
 		else:
 	    	return "OK"
 $$ LANGUAGE plpython3u;
@@ -25,6 +25,14 @@ $$ LANGUAGE plpython3u;
 CREATE TRIGGER rental_req_stamp BEFORE INSERT OR UPDATE ON rental_request
     FOR EACH STATEMENT EXECUTE FUNCTION max_rental_req_accepted_per_user();
     --- FOR EACH STATEMENT option will call the trigger function only once for each statement, regardless of the number of rows getting modified.
+
+CREATE OR REPLACE FUNCTION rental_req_disj() RETURNS trigger
+AS $$
+
+$$ LANGUAGE plpython3u
+
+CREATE TRIGGER rental_req_disj_check BEFORE INSERT OR UPDATE ON rental_request
+	FOR EACH STATEMENT EXECUTE FUNCTION rental_req_disj();
 
 
 CREATE OR REPLACE FUNCTION new_aptBlock RETURN trigger
@@ -60,7 +68,7 @@ CREATE TRIGGER insert_aptBlock_on_req_accepted AFTER UPDATE ON req_aptBlock_crea
 CREATE OR REPLACE FUNCTION define_relative_bulletinBoards RETURN trigger
 AS $$
 	aptBlock_id = TD["new"]["aptBlock_id"]
-	
+
 	qry_geneal = f"""
 			INSERT INTO aptBlock_bulletinBoard (aptBlock_id, bb_name, bb_year)
 			VALUES({aptBlock_id}, general, date_part(current_date, 'year'));
