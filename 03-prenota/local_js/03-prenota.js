1035336f90fb01edf4b1976e42e70f578626430a
rhyze1 <166700833+rhyze1@users.noreply.github.com>
2024-05-01 23:02:51 +0200

    Revert "Prenotazioni wip"
    This reverts commit a482b482a1602e4702fc742b29e7ea7d2934c02a.

--- 03-prenota/local_js/03-prenota.js
+++
@@ -1,6 +0,0 @@
-var show = function (id) {
-  return (document.getElementById(id).style.display = "grid");
-};
-var hide = function (id) {
-  return (document.getElementById(id).style.display = "none");
-};
