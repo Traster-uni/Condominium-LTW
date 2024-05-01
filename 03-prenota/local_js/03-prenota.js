a482b482a1602e4702fc742b29e7ea7d2934c02a
rhyze1 <166700833+rhyze1@users.noreply.github.com>
2024-05-01 22:47:14 +0200

    Prenotazioni wip

---
+++ 03-prenota/local_js/03-prenota.js
@@ -0,0 +1,6 @@
+var show = function (id) {
+  return (document.getElementById(id).style.display = "grid");
+};
+var hide = function (id) {
+  return (document.getElementById(id).style.display = "none");
+};
