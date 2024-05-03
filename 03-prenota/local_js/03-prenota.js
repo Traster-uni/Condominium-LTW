8dacbbc2dbbfa1368a3ab64be149dfdcf29c2114
rhyze1 <166700833+rhyze1@users.noreply.github.com>
2024-05-03 13:15:05 +0200

    prenotazioni wip

--- 03-prenota/local_js/03-prenota.js
+++ 03-prenota/local_js/03-prenota.js
@@ -1,6 +1,17 @@
 var show = function (id) {
   return (document.getElementById(id).style.display = "grid");
 };
+
 var hide = function (id) {
   return (document.getElementById(id).style.display = "none");
 };
+
+function checkTime(event) {
+  const timeStart = document.getElementById("time-start").value;
+  const timeEnd = document.getElementById("time-end").value;
+
+  if (timeEnd <= timeStart) {
+    event.preventDefault();
+    alert("L'ora di fine deve essere successiva all'ora di inizio");
+  }
+}
