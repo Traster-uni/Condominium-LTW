08b808e004f889581ec1bedc8b1898d0dd4a9a42
Rineva <78318692+Rineva@users.noreply.github.com>
2024-05-03 12:42:54 +0200

    register php update

--- 01-login/local_php/register.php
+++ 01-login/local_php/register.php
@@ -1,10 +1,13 @@
 <?php
-    $connection = pg_connect("host=localhost dbname=Condominium_LTW user=admin password=admin");
+    $connection = pg_connect("host=127.0.0.1 port=5432 dbname=Condominium_LTW user=rinaldo password=service");
+
+    //Verifico che la connessione è avvenuta con successo
     if (!$connection) {
         echo "Errore, connessione non riuscita.<br>";
         exit;
     }
 
+    //Prendo i dati dalla form e li vado ad inserire nella tabella sul DB
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $nome = htmlspecialchars($_POST["nome"]);
@@ -12,8 +15,26 @@
         $dnascita = htmlspecialchars($_POST["data-nascita"]);
         $telefono = htmlspecialchars($_POST["telefono"]);
         $fiscalcode = htmlspecialchars($_POST["fiscal-code"]);
+        $address = htmlspecialchars($_POST("address"))
         $citta = htmlspecialchars($_POST["citta"]);
         $email = htmlspecialchars($_POST["email"]);
+        $password = htmlspecialchars($_POST["password"]);
+        $password_hash = pg_escape_string(password_hash($password, PASSWORD_DEFAULT)); //Hashing della password
 
-        $q = "INSERT INTO "
+        //Preparo la query
+        $q = "INSERT INTO ut_registered(nome, cognome, d_nascita, telefono, address, citta_residenza, ut_email, passwd) VALUES ("$nome", "$cognome", "$dnascita", "$telefono", "$address", "$citta", "$email", "$password_hash")";
+        $result = pg_query($connection, $q);
+
+        // Verifica se l'inserimento è avvenuto con successo
+        if ($result) {
+            echo "Registrazione avvenuta con successo!";
+            header("Location: ../01-login.html");
+        } else {
+            echo "Errore durante la registrazione: " . pg_last_error($conn);
+        }
     }
+
+    // Chiudi la connessione al database
+    pg_close($conn);
+?>
+    
