038fe3d621dff2baf225d5d94765ce66a92d2e05
Rineva <rinaldevangelista@live.it>
2024-05-16 09:43:01 +0200

    login button fix

--- 01-login/local_js/01-login.js
+++ 01-login/local_js/01-login.js
@@ -3,15 +3,19 @@ var modalSignIn = document.getElementById("modalSignIn");
 var modalLogin = document.getElementById("modalLogin");
 
 // Get the button that opens the modal
-var btn = document.getElementById("signInButton");
-var btn = document.getElementById("loginButton");
+var btnSignIn = document.getElementById("signInButton");
+var btnLogin = document.getElementById("loginButton");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+btnSignIn.onclick = function() {
+  modalSignIn.style.display = "block";
+}
+
+btnLogin.onclick = function() {
+  modalLogin.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -21,7 +25,8 @@ span.onclick = function() {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modalSignIn || event.target == modalLogin) {
+    modalSignIn.style.display = "none";
+    modalLogin.style.display = "none";
   }
 }
