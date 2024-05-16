fbc41233b203ea5389518ded87901aed70aa0bff
Tommaso M. <84441861+Traster-uni@users.noreply.github.com>
2024-05-16 20:35:37 +0200

    updated login logic

--- 01-login/local_php/login.php
+++ 01-login/local_php/login.php
@@ -10,24 +10,47 @@
     } else {
         echo "connected";
     }
+    // may be removed as we don't use HTTPS for logon
+    session_set_cookie_params([
+        'lifetime' => 0, // Session cookie lasts until the browser is closed
+        'path' => '/',
+        'secure' => isset($_SERVER['HTTPS']), // True if using HTTPS
+        'httponly' => true, // Prevents JavaScript access to session cookie
+        'samesite' => 'Strict', // Strict or Lax depending on your needs
+    ]);
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (isset($_POST["login_button"])){
             $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
             $passwd = $_POST["password"];
-
-            $qry = "SELECT passwd
+            $qry_email = "SELECT ut_email
+                            FROM ut_registered ut_r
+                            WHERE ut_r = ut_email";
+            $qry_email_result = pg_query($connection, $qry_email);
+            if (!$qry_email_result){
+                echo "Something went wrong";
+                echo pg_result_error($qry_result);                
+            }
+            if (strcmp($qry_email_result[0]["email"], $email) != 0){
+                echo "Wrong password, try again";
+            }
+            $qry_passwd = "SELECT passwd, ut_id
                     FROM ut_registered ut_r
                     WHERE ut_r = ut_email";
-            $qry_result = pg_query($connection, $qry);
-            if (!$qry_result){
+            $qry_passwd_result = pg_query($connection, $qry_passwd);
+            if (!$qry_passwd_result){
                 echo "Something went wrong";
-                echo pg_result_error($qry_result);
+                echo pg_result_error($qry_passwd_result);
             }
-            if (strcmp($qry_result[0]["passwd"], $passwd) != 0){
+            if (strcmp($qry_passwd_result[0]["passwd"], $passwd) == 0){
+                $_SESSION["ut_id"] = $qry_passwd_result[0]["ut_id"];
+                $_SESSION["email"] = $qry_email_result[0]["email"];
+                $_SESSION["password"] = $qry_passwd_result[0]["passwd"];
+            } else {
                 echo "Wrong password, try again";
             }
         }
     }
 
     pg_close($connection); // close connection
+    session_regenerate_id(true);
