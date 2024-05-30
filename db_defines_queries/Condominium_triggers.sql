cf9c493d50a5edaff1f31ddda27cc25e7e85fd42
Traster-uni <lopedote.1942920@studenti.uniroma1.it>
2024-05-30 10:32:51 +0200

    Update Condominium_triggers.sql

--- db_defines_queries/Condominium_triggers.sql
+++ db_defines_queries/Condominium_triggers.sql
@@ -141,13 +141,13 @@ CREATE OR REPLACE TRIGGER insert_bulletinBoard_on_aptBlock_creation
 CREATE OR REPLACE FUNCTION timestamp_update_on_update_ticket() RETURNS trigger
 AS $$ 
 	t_name = TD["table_name"]
-	p_id = TD["new"]["ticket_id"]
+	tick_id = TD["new"]["ticket_id"]
 	t_status_new = TD["new"]["status"]
 	t_status_old = TD["old"]["status"]
 	qry = f"""
 			UPDATE {t_name}
 			SET time_mod = NOW()::timestamp
-			WHERE {t_name}.post_id = {p_id}
+			WHERE {t_name}.ticket_id = {tick_id}
 			"""
 
 	if t_status_old == 'open':
